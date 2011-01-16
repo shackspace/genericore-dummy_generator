2b3ae7c753d9af442209de32576ed745014ddd64
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-01-16 21:30:30 +0100

    added more controlling parameters

--- main.py
+++ main.py
@@ -8,24 +8,31 @@ log = logging.getLogger('dummy_generator')
 
 parser = argparse.ArgumentParser(description='generates dummy package on given exchange against AMQP')
 parser.add_argument('--host',default='141.31.8.11',      help='AMQP host ip address')
-#parser.add_argument('--port',type=int,default=5672,      help='AMQP host port')
-parser.add_argument('-u','--username',default='guest',   help='AMQP username') 
+parser.add_argument('--port',type=int,default=5672,      help='AMQP host port')
+parser.add_argument('-u','--username',default='shack',   help='AMQP username') 
 parser.add_argument('-p','--password',default='guest',   help='AMQP password') 
 parser.add_argument('-e','--exchange',default='mail_src',help='AMQP Exchange to publish in') 
 
 parser.add_argument('-f','--file',default=sys.stdin,type=argparse.FileType('r'),help='payload to send to exchange, default is STDIN') 
 
-parser.add_argument('-t','--timeout',type=int,default=9999999,help='timeout in seconds between generation cycles') 
+parser.add_argument('-t','--type',default='fanout',help='AMQP Exchange type') 
+parser.add_argument('-b','--heartbeat',type=int,default=0,help='AMQP Heartbeat value') 
+parser.add_argument('-v','--vhost',default='/',help='AMQP vhost definition') 
+
+parser.add_argument('-r','--repeat',type=int,default=0,help='repeat message after N seconds cycles') 
 args = parser.parse_args()
 log.info ("Parameters %s" % args)
 
 connection = pika.AsyncoreConnection(pika.ConnectionParameters(
           credentials = pika.PlainCredentials(args.username,args.password), 
-          host=args.host))
+          host=args.host, port=args.port,
+          heartbeat=args.heartbeat,
+          virtual_host=args.vhost,
+          ))
 channel = connection.channel()
 
 channel.exchange_declare(exchange=args.exchange,
-                             type='fanout')
+                             type=args.type)
 
 
 log.info('Reading payload from file')
@@ -36,8 +43,8 @@ while True:
   channel.basic_publish(exchange=args.exchange,
       routing_key='',
       body=data)
+  if args.repeat == 0:
+     exit(0)
   log.info('Waiting %d for next publish' %args.timeout)
   time.sleep(args.timeout)
 
-
-pika.asyncore_loop()
